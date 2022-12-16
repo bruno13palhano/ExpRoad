a7c3f3bc6ddaf854572a81c8413c2887ca7a9c92
bruno13palhano <bruno13palhano@gmail.com>
2022-12-15 22:14:32 -0300

    Extracted queries to a specific Dao

---
+++ app/activity_repository/src/main/java/com/bruno13palhano/activity_repository/dao/DailyActivityAnalyticsDao.kt
@@ -0,0 +1,17 @@
+package com.bruno13palhano.activity_repository.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Query
+import com.bruno13palhano.activity_model.DailyActivity
+import java.util.*
+
+@Dao
+internal interface DailyActivityAnalyticsDao {
+
+    @Query("SELECT * FROM daily_activity_table WHERE activity_title LIKE '%' || :activityTitle || '%'")
+    fun getAnalyticsByTitle(activityTitle: String): LiveData<List<DailyActivity>>
+
+    @Query("SELECT * FROM daily_activity_table WHERE activity_date BETWEEN :initialDate AND :finalDate")
+    fun getAnalyticsByDate(initialDate: Date, finalDate: Date): LiveData<List<DailyActivity>>
+}
