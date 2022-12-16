94140bf776b7730f20361c39795e6c0dffb08331
bruno13palhano <bruno13palhano@gmail.com>
2022-12-15 22:23:42 -0300

    Changed dao variable to DailyActivityAnalyticsDao

--- app/activity_repository/src/main/java/com/bruno13palhano/activity_repository/DailyActivityRepositoryFactory.kt
+++ app/activity_repository/src/main/java/com/bruno13palhano/activity_repository/DailyActivityRepositoryFactory.kt
@@ -6,13 +6,13 @@ import com.bruno13palhano.activity_repository.dao.DailyActivityDatabase
 class DailyActivityRepositoryFactory(
     private val context: Context
 ) {
-    private val dao = DailyActivityDatabase.getInstance(context).dailyActivityDao
-
     fun createDailyActivityRepositoryDB(): DailyActivityRepository {
+        val dao = DailyActivityDatabase.getInstance(context).dailyActivityDao
         return DailyActivityRepositoryDBImpl(dao)
     }
 
     fun createDailyActivityAnalyticsRepositoryDB(): DailyActivityAnalyticsRepository {
+        val dao = DailyActivityDatabase.getInstance(context).dailyActivityAnalyticsDao
         return DailyActivityAnalyticsRepositoryDBImpl(dao)
     }
 }
