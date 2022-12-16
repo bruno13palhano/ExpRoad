7a05ff86c5d54c6a9a388ff163f4edeb0c7d4fd5
bruno13palhano <bruno13palhano@gmail.com>
2022-12-15 22:21:21 -0300

    Changed dao variable to DailyActivityAnalyticsDao

--- app/activity_repository/src/main/java/com/bruno13palhano/activity_repository/DailyActivityAnalyticsRepositoryDBImpl.kt
+++ app/activity_repository/src/main/java/com/bruno13palhano/activity_repository/DailyActivityAnalyticsRepositoryDBImpl.kt
@@ -2,18 +2,19 @@ package com.bruno13palhano.activity_repository
 
 import androidx.lifecycle.LiveData
 import com.bruno13palhano.activity_model.DailyActivity
+import com.bruno13palhano.activity_repository.dao.DailyActivityAnalyticsDao
 import com.bruno13palhano.activity_repository.dao.DailyActivityDao
 import java.util.*
 
 internal class DailyActivityAnalyticsRepositoryDBImpl(
-    private val dao: DailyActivityDao
+    private val dao: DailyActivityAnalyticsDao
 ) : DailyActivityAnalyticsRepository {
 
     override fun getByTitle(activityTitle: String): LiveData<List<DailyActivity>> {
-        return dao.getByTitle(activityTitle)
+        return dao.getAnalyticsByTitle(activityTitle)
     }
 
     override fun getByDate(initialDate: Date, finalDate: Date): LiveData<List<DailyActivity>> {
-        return dao.getByDate(initialDate, finalDate)
+        return dao.getAnalyticsByDate(initialDate, finalDate)
     }
 }
