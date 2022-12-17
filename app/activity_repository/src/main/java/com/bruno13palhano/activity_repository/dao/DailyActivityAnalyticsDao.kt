0469e7d047f1b090c504d2f11b7c64522888ef3e
bruno13palhano <bruno13palhano@gmail.com>
2022-12-16 23:44:58 -0300

    Added new methods

--- app/activity_repository/src/main/java/com/bruno13palhano/activity_repository/dao/DailyActivityAnalyticsDao.kt
+++ app/activity_repository/src/main/java/com/bruno13palhano/activity_repository/dao/DailyActivityAnalyticsDao.kt
@@ -12,6 +12,12 @@ internal interface DailyActivityAnalyticsDao {
     @Query("SELECT * FROM daily_activity_table WHERE activity_title LIKE '%' || :activityTitle || '%'")
     fun getAnalyticsByTitle(activityTitle: String): LiveData<List<DailyActivity>>
 
+    @Query("SELECT * FROM daily_activity_table WHERE activity_type LIKE '%' || :activityType || '%'")
+    fun getAnalyticsByType(activityType: String): LiveData<List<DailyActivity>>
+
+    @Query("SELECT * FROM daily_activity_table WHERE activity_description LIKE '%' || :activityDescription || '%'")
+    fun getAnalyticsByDescription(activityDescription: String)
+
     @Query("SELECT * FROM daily_activity_table WHERE activity_date BETWEEN :initialDate AND :finalDate")
     fun getAnalyticsByDate(initialDate: Date, finalDate: Date): LiveData<List<DailyActivity>>
 }
