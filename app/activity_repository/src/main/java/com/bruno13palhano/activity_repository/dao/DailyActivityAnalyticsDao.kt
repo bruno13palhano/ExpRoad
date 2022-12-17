7135a3d5befa20f2bd667ed71f939d9ba68bf92f
bruno13palhano <bruno13palhano@gmail.com>
2022-12-17 00:00:08 -0300

    Added new methods

--- app/activity_repository/src/main/java/com/bruno13palhano/activity_repository/dao/DailyActivityAnalyticsDao.kt
+++ app/activity_repository/src/main/java/com/bruno13palhano/activity_repository/dao/DailyActivityAnalyticsDao.kt
@@ -16,8 +16,13 @@ internal interface DailyActivityAnalyticsDao {
     fun getAnalyticsByType(activityType: String): LiveData<List<DailyActivity>>
 
     @Query("SELECT * FROM daily_activity_table WHERE activity_description LIKE '%' || :activityDescription || '%'")
-    fun getAnalyticsByDescription(activityDescription: String)
+    fun getAnalyticsByDescription(activityDescription: String): LiveData<List<DailyActivity>>
 
     @Query("SELECT * FROM daily_activity_table WHERE activity_date BETWEEN :initialDate AND :finalDate")
     fun getAnalyticsByDate(initialDate: Date, finalDate: Date): LiveData<List<DailyActivity>>
+
+    @Query("SELECT * FROM daily_activity_table daily1 WHERE 9=(" +
+            "SELECT COUNT(DISTINCT(daily2.activity_time)) FROM daily_activity_table daily2 WHERE " +
+            "daily2.activity_time > daily1.activity_time)")
+    fun getTop10Hours(): LiveData<List<DailyActivity>>
 }
