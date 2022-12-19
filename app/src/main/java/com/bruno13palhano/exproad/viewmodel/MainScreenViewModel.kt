9fcfc2131c5df6890015902d39b8ae0fca3cfb04
bruno13palhano <bruno13palhano@gmail.com>
2022-12-19 17:31:04 -0300

    Changed the return type to DailyActivity

--- app/src/main/java/com/bruno13palhano/exproad/viewmodel/MainScreenViewModel.kt
+++ app/src/main/java/com/bruno13palhano/exproad/viewmodel/MainScreenViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.bruno13palhano.activity_model.DailyActivity
 import com.bruno13palhano.activity_repository.DailyActivityRepository
+import com.bruno13palhano.activity_repository.model.DailyActivityImpl
 
 class MainScreenViewModel(
     private val repository: DailyActivityRepository
