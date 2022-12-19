339d90aa0d7c1a9d0d833a9d1570128c1a4ae1b5
bruno13palhano <bruno13palhano@gmail.com>
2022-12-19 19:07:55 -0300

    Added default time

--- app/activity_model/src/main/java/com/bruno13palhano/activity_model/DailyActivity.kt
+++ app/activity_model/src/main/java/com/bruno13palhano/activity_model/DailyActivity.kt
@@ -1,30 +1,12 @@
 package com.bruno13palhano.activity_model
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 import java.util.*
 
-@Entity(tableName = "daily_activity_table")
 data class DailyActivity(
-
-    @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "activity_id")
-    var activityId: Long = 0L,
-
-    @ColumnInfo(name = "activity_title")
-    var activityTitle: String,
-
-    @ColumnInfo(name = "activity_type")
-    var activityType: String,
-
-    @ColumnInfo(name = "activity_description")
-    var activityDescription: String = "",
-
-    @ColumnInfo(name = "activity_time")
-    var activityTime: Long = 10800000L,
-
-    @ColumnInfo(name = "activity_date")
-    var activityDate: Date = Date(),
-
+    val activityId: Long = 0L,
+    val activityTitle: String,
+    val activityType: String,
+    val activityDescription: String = "",
+    val activityTime: Long = defaultTime,
+    val activityDate: Date = Date(),
 )
--- app/activity_model/src/main/java/com/bruno13palhano/activity_model/Utils.kt
+++ app/activity_model/src/main/java/com/bruno13palhano/activity_model/Utils.kt
@@ -1,2 +1,3 @@
 package com.bruno13palhano.activity_model
 
+const val defaultTime = 10800000L
